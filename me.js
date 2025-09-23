92f6ed9d80933e85e43412d50f844dec244b1af6
Mounib Mammeri <bm.mammeri@ensta.edu.dz>
2025-09-23 22:18:31 +0100

    Update me.js

--- me.js
+++ me.js
@@ -37,3 +37,27 @@ window.addEventListener('scroll', revealOnScroll);
 
 // Trigger once on page load
 revealOnScroll();
+
+// Mobile menu toggle
+const menuButton = document.querySelector('.menu-dots');
+const mobileMenu = document.getElementById('mobileMenu');
+if (menuButton && mobileMenu) {
+  menuButton.addEventListener('click', () => {
+    const isHidden = mobileMenu.hasAttribute('hidden');
+    if (isHidden) {
+      mobileMenu.removeAttribute('hidden');
+      menuButton.setAttribute('aria-expanded', 'true');
+    } else {
+      mobileMenu.setAttribute('hidden', '');
+      menuButton.setAttribute('aria-expanded', 'false');
+    }
+  });
+
+  // Close menu on link click
+  mobileMenu.querySelectorAll('a').forEach(a => {
+    a.addEventListener('click', () => {
+      mobileMenu.setAttribute('hidden', '');
+      menuButton.setAttribute('aria-expanded', 'false');
+    });
+  });
+}
